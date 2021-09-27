ca15a38b68c76af144181902c0fadd77260e90ff
David Perez Antonio <david.perez.antonio51@gmail.com>
2021-09-27 11:10:54 -0500

    prueba de testbench

---
+++ adderlib8bit00B/testbench/sim_para.tcl
@@ -0,0 +1,23 @@
+lappend auto_path "D:/Programas/lscc/diamond/3.12/data/script"
+package require simulation_generation
+set ::bali::simulation::Para(DEVICEFAMILYNAME) {MachXO2}
+set ::bali::simulation::Para(PROJECT) {testbench}
+set ::bali::simulation::Para(PROJECTPATH) {D:/GIT/Arquitectura_de_Computadoras/adderlib8bit00B}
+set ::bali::simulation::Para(FILELIST) {"D:/GIT/Arquitectura_de_Computadoras/adderlib8bit00B/testbench/adderlib8bit00.vhdl" "D:/GIT/Arquitectura_de_Computadoras/adderlib8bit00B/testbench/testbenchadderlib8bit00.vhdl" }
+set ::bali::simulation::Para(GLBINCLIST) {}
+set ::bali::simulation::Para(INCLIST) {"none" "none"}
+set ::bali::simulation::Para(WORKLIBLIST) {"work" "work" }
+set ::bali::simulation::Para(COMPLIST) {"VHDL" "VHDL" }
+set ::bali::simulation::Para(SIMLIBLIST) {pmi_work ovi_machxo2}
+set ::bali::simulation::Para(MACROLIST) {}
+set ::bali::simulation::Para(SIMULATIONTOPMODULE) {testbenchadderlib8bit00}
+set ::bali::simulation::Para(SIMULATIONINSTANCE) {}
+set ::bali::simulation::Para(LANGUAGE) {VHDL}
+set ::bali::simulation::Para(SDFPATH)  {}
+set ::bali::simulation::Para(INSTALLATIONPATH) {D:/Programas/lscc/diamond/3.12}
+set ::bali::simulation::Para(ADDTOPLEVELSIGNALSTOWAVEFORM)  {1}
+set ::bali::simulation::Para(RUNSIMULATION)  {1}
+set ::bali::simulation::Para(HDLPARAMETERS) {}
+set ::bali::simulation::Para(POJO2LIBREFRESH)    {}
+set ::bali::simulation::Para(POJO2MODELSIMLIB)   {}
+::bali::simulation::ModelSim_Run
